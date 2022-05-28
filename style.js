65efe1c8bc7b4ce7ca025122b3bbfd827a948532
Gokul Raj <106130828+Gokulcasper@users.noreply.github.com>
2022-05-28 18:49:43 +0530

    For & While

---
+++ style.js
@@ -0,0 +1,22 @@
+// for(let i=0;i<=10;i++)
+// {
+//     if(i%2 !==0)
+//     {
+//         console.log("Odd Nunber : ", i);
+//     }
+// }
+
+
+
+//while Condition
+
+
+let i=0;
+while(i<=10)
+{
+    if(i%2 !==0)
+    {
+        console.log("Odd Nunber : ", i);
+    }
+    i++
+}
